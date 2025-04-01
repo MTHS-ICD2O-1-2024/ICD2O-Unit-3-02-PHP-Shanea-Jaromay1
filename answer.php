f58ac28255fa7a845e0e21b635b48e44c9c11bb8
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-01 16:43:00 +0000

    init

---
+++ answer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Volume of a Pyramid, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Shanea Jaromay" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-amber.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="./site.webmanifest" />
+  <title>Volume of a Pyramid, in PHP</title>
+</head>
+
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Volume of a Pyramid, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/pyramid.png" alt="pyramid image" width="300" />
+      </div>
+      <div class="page-content-php">
+        <div id="area-info">
+          <?php
+          // input
+          $lengthOfPyramid = $_GET["length - of - pyramid"];
+          $widthOfPyramid = $_GET["width-of-pyramid"];
+          $heightOfPyramid = $_GET["height-of-pyramid"];
+
+          // process
+          $volume = ($lengthOfPyramid * $widthOfPyramid * $heightOfPyramid) / 3;
+
+          // output
+          echo "Your volume will be: " . $volume . "mm³";
+          ?>
+        </div>
+      </div>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
